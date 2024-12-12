9ea5349f5e91ef38c2b32ad4e6e44da7634c13bb
Anthony Nero <software_developer12@outlook.com>
2024-12-12 15:29:11 -0500

    add increment and decrementing rules to counter

--- src/app/counter-lab/pages/ui.component.ts
+++ src/app/counter-lab/pages/ui.component.ts
@@ -1,4 +1,9 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  signal,
+  computed,
+} from '@angular/core';
 
 @Component({
   selector: 'app-counter-ui',
@@ -6,11 +11,28 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   imports: [],
   template: `
     <div>
-      <button class="btn btn-primary">-</button>
-      <span data-testid="current">0</span>
-      <button class="btn btn-primary">+</button>
+      <button
+        [disabled]="disableDecrement()"
+        (click)="decrement()"
+        class="btn btn-primary"
+      >
+        -
+      </button>
+      <span data-testid="current">{{ current() }}</span>
+      <button (click)="increment()" class="btn btn-primary">+</button>
     </div>
   `,
   styles: ``,
 })
-export class UiComponent {}
+export class UiComponent {
+  current = signal(0);
+
+  increment() {
+    this.current.update(() => this.current() + 1);
+  }
+  decrement() {
+    this.current.update(() => this.current() - 1);
+  }
+
+  disableDecrement = computed(() => this.current() === 0);
+}
