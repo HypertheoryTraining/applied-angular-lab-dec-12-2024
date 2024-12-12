711b8324279ac4034c08e800165d21daec4f21fb
Alec <alec_b_stringer@progressive.com>
2024-12-12 15:41:22 -0500

    counter lab - fizzbuzz

--- src/app/counter-lab/pages/ui.component.ts
+++ src/app/counter-lab/pages/ui.component.ts
@@ -16,6 +16,7 @@ import {
       </button>
       <span data-testid="current">{{ count() }}</span>
       <button class="btn btn-primary" (click)="inc()">+</button>
+      <p data-testid="fizzBuzz">{{ fizzBuzz() }}</p>
     </div>
   `,
   styles: ``,
@@ -29,5 +30,18 @@ export class UiComponent {
   }
   inc() {
     this.count.update((n) => n + 1);
+  }
+  fizzBuzz() {
+    if (this.count() === 0) {
+      return '';
+    } else if (this.count() % 3 === 0 && this.count() % 5 === 0) {
+      return 'fizzbuzz';
+    } else if (this.count() % 3 === 0) {
+      return 'fizz';
+    } else if (this.count() % 5 === 0) {
+      return 'buzz';
+    } else {
+      return '';
+    }
   }
 }
