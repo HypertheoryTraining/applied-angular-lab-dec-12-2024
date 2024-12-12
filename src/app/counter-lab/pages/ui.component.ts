c56f889d1c65364725498b9a7bc6a2ec5e537d08
weam <weam_mohamed_2@progressive.com>
2024-12-12 16:02:29 -0500

    Implemented FizzBuzz functionality in UI component

--- src/app/counter-lab/pages/ui.component.ts
+++ src/app/counter-lab/pages/ui.component.ts
@@ -19,4 +19,13 @@ export class UiComponent {
       this.count.update((value) => value - 1);
     }
   }
+
+  get fizzBuzz() {
+    const value = this.count();
+    if (value === 0) return '';
+    if (value % 3 === 0 && value % 5 === 0) return 'FizzBuzz';
+    if (value % 3 === 0) return 'Fizz';
+    if (value % 5 === 0) return 'Buzz';
+    return '';
+  }
 }
