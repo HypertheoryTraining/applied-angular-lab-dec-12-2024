396de7de53fff063a4b379e4868f0071f98260f4
Madison Hengstebeck <madison_hengstebeck@progressive.com>
2024-12-12 14:37:48 -0500

    display list of books in table

--- src/app/features/books/books.component.ts
+++ src/app/features/books/books.component.ts
@@ -1,10 +1,56 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, ChangeDetectionStrategy, resource } from '@angular/core';
 
+export type BookEntity = {
+  author: string;
+  country: string;
+  imageLink: string;
+  language: string;
+  link: string;
+  pages: number;
+  title: string;
+  year: number;
+  id: string;
+};
+
+type BookApiResponse = {
+  data: BookEntity[];
+};
 @Component({
   selector: 'app-books',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
-  template: ``,
+  imports: [JsonPipe],
+  template: `
+    <div className="overflow-x-auto">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          @for (book of books.value(); track book.id) {
+            <tr>
+              <td>{{ book.id }}</td>
+              <td>{{ book.title }}</td>
+              <td>{{ book.author }}</td>
+              <td>{{ book.year }}</td>
+            </tr>
+          }
+        </tbody>
+      </table>
+    </div>
+  `,
   styles: ``,
 })
-export class BooksComponent {}
+export class BooksComponent {
+  books = resource<BookEntity[], unknown>({
+    loader: () =>
+      fetch('/api/books')
+        .then((res) => res.json())
+        .then((r) => r.data),
+  });
+}
