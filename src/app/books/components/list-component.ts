7e0ec01ab930433040ce24f029d0529a59f9a9d4
Jeffry Gonzalez <jeff@hypertheory.com>
2024-12-12 14:29:43 -0500

    books displayed in grid

--- src/app/books/components/list-component.ts
+++ src/app/books/components/list-component.ts
@@ -1,18 +1,35 @@
-import { JsonPipe } from '@angular/common';
-import { Component, ChangeDetectionStrategy, resource } from '@angular/core';
+import { ChangeDetectionStrategy, Component, resource } from '@angular/core';
 
+type BookEntity = {
+  id: string;
+  title: string;
+  author: string;
+  year: number;
+};
 @Component({
   selector: 'app-book-list',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [JsonPipe],
-  template: `
-    <p>book list</p>
-    <pre>{{ books.value() | json }}</pre>
-  `,
+  imports: [],
+  template: ` @if (books.hasValue()) {
+    <div class="grid grid-cols-4 gap-8">
+      @for (book of books.value(); track book.id) {
+        <div class="card bg-base-100  shadow-xl">
+          <div class="card-body">
+            <h2 class="card-title text-green-600">{{ book.title }}</h2>
+            <p>{{ book.title }} is by {{ book.author }}.</p>
+            <p>It was released in {{ book.year }} by {{ book.author }}</p>
+          </div>
+        </div>
+      }
+    </div>
+  }`,
   styles: ``,
 })
 export class ListComponent {
-  books = resource({
-    loader: () => fetch('/api/books').then((r) => r.json()),
+  books = resource<BookEntity[], unknown>({
+    loader: () =>
+      fetch('/api/books')
+        .then((r) => r.json())
+        .then((r) => r.data),
   });
 }
