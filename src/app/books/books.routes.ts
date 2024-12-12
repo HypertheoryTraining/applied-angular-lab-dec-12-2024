ab4ea95498475b2c2a6cda06eea04727f78f727c
Jeffry Gonzalez <jeff@hypertheory.com>
2024-12-12 14:54:43 -0500

    switched to store

--- src/app/books/books.routes.ts
+++ src/app/books/books.routes.ts
@@ -2,10 +2,13 @@ import { Routes } from '@angular/router';
 import { BooksComponent } from './books.component';
 import { ListComponent } from './components/list-component';
 import { StatsComponent } from './components/stats-component';
+import { BooksDataService } from './services/books-data.service';
+import { BookStore } from './services/books.store';
 
 export const BOOK_ROUTES: Routes = [
   {
     path: '',
+    providers: [BooksDataService, BookStore],
     component: BooksComponent,
     children: [
       { path: 'list', component: ListComponent },
--- src/app/books/services/books.store.ts
+++ src/app/books/services/books.store.ts
@@ -1,3 +1,51 @@
-import { signalStore, withState } from '@ngrx/signals';
+import {
+  patchState,
+  signalStore,
+  withComputed,
+  withHooks,
+  withMethods,
+  withState,
+} from '@ngrx/signals';
+import { setEntities, withEntities } from '@ngrx/signals/entities';
+import { BookEntity } from '../types';
+import { computed, inject } from '@angular/core';
+import { BooksDataService } from './books-data.service';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
+import { pipe, switchMap } from 'rxjs';
+import { tapResponse } from '@ngrx/operators';
 
-export const BookStore = signalStore(withState({}));
+export const BookStore = signalStore(
+  withEntities<BookEntity>(),
+  withComputed((store) => {
+    return {
+      books: computed(() => store.entities()),
+    };
+  }),
+  withMethods((store) => {
+    const service = inject(BooksDataService);
+
+    return {
+      _load: rxMethod<void>(
+        pipe(
+          switchMap(() =>
+            service.getBooks().pipe(
+              tapResponse({
+                next(value) {
+                  patchState(store, setEntities(value));
+                },
+                error(error) {
+                  console.error(error);
+                },
+              }),
+            ),
+          ),
+        ),
+      ),
+    };
+  }),
+  withHooks({
+    onInit(store) {
+      store._load();
+    },
+  }),
+);
